d390aea5ade7daf11a41cfe1d5a29a2abb8c7821
Dimakdrshv <147543834+Dimakdrshv@users.noreply.github.com>
2024-11-22 17:34:27 +0300

    Delete ProjectException.cpp

--- ProjectException.cpp
+++
@@ -1,9 +0,0 @@
-#include "ProjectException.h"
-
-ProjectException::ProjectException(std::string msg) {
-    this->msg = msg;
-}
-
-const char* ProjectException::what() const noexcept {
-    return this->msg.c_str();
-}
